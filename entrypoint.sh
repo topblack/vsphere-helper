42841aaf5d3fbc7cd763b714a1c41e5cc9c255be
Leon Qin <qinling.net@gmail.com>
2020-12-28 21:49:47 +0800

    Fix s3fs credential file

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,11 @@ WORKDIR=$PWD
 DATADIR=$WORKDIR/data
 
 if [ "x$USE_S3_BUCKET" != "x" ]; then
+    PASSFILE=${HOME}/.passwd-s3fs
     mkdir /data
-    s3fs $USE_S3_BUCKET /data
+    echo $ACCESS_KEY_ID:$SECRET_ACCESS_KEY > ${PASSFILE}
+    chmod 600 ${PASSFILE}
+    s3fs $USE_S3_BUCKET /data -o passwd_file=${PASSFILE}
     DATADIR=/data
 fi
 
